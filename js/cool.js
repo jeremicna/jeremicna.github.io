324e5a0acfde32b7be1e8fe7c4d129a8bde0de99
FruitBarrel <jeremicampagna@outlook.com>
2021-09-17 23:28:25 -0400

    com

--- js/cool.js
+++ js/cool.js
@@ -4,14 +4,14 @@ const Network = opensea.Network
 const Web3 = require("web3")
 
 
-const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io')
+const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/59938e653dd147b08f830faa2deda492')
 
 
 const seaport = new OpenSeaPort(provider, {
   networkName: Network.Main
 })
 
-const accountAddress = "0x59a5f50c788579d73dbd8d4e56a5a262783ddccf"
+const accountAddress = "0xC2d714611B8d490aB21AF2E35cEdeAB10bb53fDd"
 
 const asset = {
     tokenAddress: "0x60e4d786628fea6478f785a6d7e704777c86a7c6", // CryptoKitties
@@ -19,11 +19,16 @@ const asset = {
 }
 
 async function main() {
-    const balance = await seaport.getAssetBalance({
-        accountAddress, // string
-        asset, // Asset
+    const offer = await seaport.createBuyOrder({
+        asset: {
+            tokenAddress: "0x60e4d786628fea6478f785a6d7e704777c86a7c6", // CryptoKitties
+            tokenId: "1830", // Token ID
+        },
+        accountAddress,
+        // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
+        startAmount: 1.2,
     })
-    console.log(balance.greaterThan(0))
+    console.log(offer)
 }
 
 window.onload = function(){
