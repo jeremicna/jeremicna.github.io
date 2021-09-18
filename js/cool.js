b93b748f96e02b5e6587c5deb5968385ce153538
FruitBarrel <jeremicampagna@outlook.com>
2021-09-18 12:40:15 -0400

    com

--- js/cool.js
+++ js/cool.js
@@ -34,6 +34,61 @@ async function main() {
         expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
     })
     console.log(offer)
+    const offer = await seaport.createBuyOrder({
+        asset: {
+            tokenAddress: "0x60e4d786628fea6478f785a6d7e704777c86a7c6", // CryptoKitties
+            tokenId: "1831", // Token ID
+        },
+        accountAddress,
+        // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
+        startAmount: 0.0,
+        expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
+    })
+    console.log(offer)
+    const offer = await seaport.createBuyOrder({
+        asset: {
+            tokenAddress: "0x60e4d786628fea6478f785a6d7e704777c86a7c6", // CryptoKitties
+            tokenId: "1832", // Token ID
+        },
+        accountAddress,
+        // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
+        startAmount: 0.0,
+        expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
+    })
+    console.log(offer)
+    const offer = await seaport.createBuyOrder({
+        asset: {
+            tokenAddress: "0x60e4d786628fea6478f785a6d7e704777c86a7c6", // CryptoKitties
+            tokenId: "1833", // Token ID
+        },
+        accountAddress,
+        // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
+        startAmount: 0.0,
+        expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
+    })
+    console.log(offer)
+    const offer = await seaport.createBuyOrder({
+        asset: {
+            tokenAddress: "0x60e4d786628fea6478f785a6d7e704777c86a7c6", // CryptoKitties
+            tokenId: "1834", // Token ID
+        },
+        accountAddress,
+        // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
+        startAmount: 0.0,
+        expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
+    })
+    console.log(offer)
+    const offer = await seaport.createBuyOrder({
+        asset: {
+            tokenAddress: "0x60e4d786628fea6478f785a6d7e704777c86a7c6", // CryptoKitties
+            tokenId: "1835", // Token ID
+        },
+        accountAddress,
+        // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
+        startAmount: 0.0,
+        expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
+    })
+    console.log(offer)
 }
 
 window.onload = function(){
