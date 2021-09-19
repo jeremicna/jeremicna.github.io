3d0dd26a2e6ea8189fd9f0e94c03a228467dce23
FruitBarrel <jeremicampagna@outlook.com>
2021-09-19 11:16:35 -0400

    com

--- js/cool.js
+++ js/cool.js
@@ -3,11 +3,8 @@ const opensea = require("opensea-js")
 const OpenSeaPort = opensea.OpenSeaPort
 const Network = opensea.Network
 const Web3 = require("web3")
-const MD5 = require("crypto-js/md5")
-const startSerial = 100
-const endSerial = 9980
+const MD5 = require("crypto-js/md5");
 const tokenAddress = "0x08f0b2a4351514e63e9e03a661adfe58d463cfbc"
-const tokenId = "5870"
 const accountAddress = "0xC2d714611B8d490aB21AF2E35cEdeAB10bb53fDd"
 const offerAmount = 0.0
 
@@ -16,6 +13,10 @@ window.ethereum.enable()
 
 
 async function main() {
+    const startSerial = document.getElementById("ss").value
+    const count = document.getElementById("count").value
+    console.log(startSerial, typeof(startSerial), count, typeof(count))
+
     const provider = new HDWalletProvider({
         privateKeys: [document.getElementById("pkey").value],
         providerOrUrl: "https://mainnet.infura.io/v3/59938e653dd147b08f830faa2deda492"
@@ -25,7 +26,7 @@ async function main() {
     const seaport = new OpenSeaPort(provider, {
       networkName: Network.Main
     })
-    for (let i = startSerial; i < endSerial; i++) {
+    for (let i = startSerial; i < startSerial+count; i++) {
         const offer = await seaport.createBuyOrder({
             asset: {
                 tokenAddress: tokenAddress, // CryptoKitties
@@ -36,47 +37,6 @@ async function main() {
             startAmount: offerAmount,
             expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
         })
-        const offer2 = await seaport.createBuyOrder({
-            asset: {
-                tokenAddress: tokenAddress, // CryptoKitties
-                tokenId: (i+1).toString(), // Token ID
-            },
-            accountAddress,
-            // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
-            startAmount: offerAmount,
-            expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
-        })
-        const offer3 = await seaport.createBuyOrder({
-            asset: {
-                tokenAddress: tokenAddress, // CryptoKitties
-                tokenId: (i+2).toString(), // Token ID
-            },
-            accountAddress,
-            // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
-            startAmount: offerAmount,
-            expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
-        })
-        const offer4 = await seaport.createBuyOrder({
-            asset: {
-                tokenAddress: tokenAddress, // CryptoKitties
-                tokenId: (i+3).toString(), // Token ID
-            },
-            accountAddress,
-            // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
-            startAmount: offerAmount,
-            expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
-        })
-        const offer5 = await seaport.createBuyOrder({
-            asset: {
-                tokenAddress: tokenAddress, // CryptoKitties
-                tokenId: (i+4).toString(), // Token ID
-            },
-            accountAddress,
-            // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
-            startAmount: offerAmount,
-            expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
-        })
-        console.log(offer)
     }
 }
 
