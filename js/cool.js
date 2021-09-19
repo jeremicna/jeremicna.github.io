b015413b8a839393a90420add1975464df9a2bf6
FruitBarrel <jeremicampagna@outlook.com>
2021-09-18 21:54:40 -0400

    com

--- js/cool.js
+++ js/cool.js
@@ -3,6 +3,11 @@ const OpenSeaPort = opensea.OpenSeaPort
 const Network = opensea.Network
 const Web3 = require("web3")
 const MD5 = require("crypto-js/md5")
+const startSerial = 100
+const endSerial = 9980
+const tokenAddress = "0x60e4d786628fea6478f785a6d7e704777c86a7c6"
+const tokenId = "658"
+const accountAddress = "0xc2d714611b8d490ab21af2e35cedeab10bb53fdd"
 
 
 window.ethereum.enable()
@@ -15,25 +20,21 @@ const seaport = new OpenSeaPort(provider, {
   networkName: Network.Main
 })
 
-const accountAddress = "0xC2d714611B8d490aB21AF2E35cEdeAB10bb53fDd"
-
-const asset = {
-    tokenAddress: "0xc9cb0fee73f060db66d2693d92d75c825b1afdbf", // CryptoKitties
-    tokenId: "325", // Token ID
-}
 
 async function main() {
-    const offer = await seaport.createBuyOrder({
-        asset: {
-            tokenAddress: "0xc9cb0fee73f060db66d2693d92d75c825b1afdbf", // CryptoKitties
-            tokenId: "325", // Token ID
-        },
-        accountAddress,
-        // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
-        startAmount: 0.0,
-        expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
-    })
-    console.log(offer)
+    //for (let i = startSerial; i < endSerial; i++) {
+        const offer = await seaport.createBuyOrder({
+            asset: {
+                tokenAddress: tokenAddress, // CryptoKitties
+                tokenId: tokenId, // Token ID
+            },
+            accountAddress,
+            // Value of the offer, in units of the payment token (or wrapped ETH if none is specified):
+            startAmount: 0.00,
+            expirationTime: Math.round(Date.now() / 1000 + 60 * 60)
+        })
+        console.log(offer)
+    //}
 }
 
 window.onload = function(){
