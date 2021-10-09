c63b8a4a22b7324d3f9767e4fdfcf76420f7277c
FruitBarrel <jeremicampagna@outlook.com>
2021-10-08 23:37:40 -0400

    com

--- js/cool.js
+++ js/cool.js
@@ -4,7 +4,8 @@ const OpenSeaPort = opensea.OpenSeaPort
 const Network = opensea.Network
 const Web3 = require("web3")
 const MD5 = require("crypto-js/md5");
-const accountAddress = ""
+var accountAddress = ""
+var alchemy = ""
 var active = true
 
 
@@ -17,6 +18,7 @@ window.ethereum.enable()
 
 
 async function main() {
+    console.log("STARTED PISSMODE")
     const targetSerials = document.getElementById("serials").value.split(" ")
     console.log(targetSerials.length)
     const tokenAddress = document.getElementById("colltoken").value
@@ -28,7 +30,7 @@ async function main() {
 
     const provider = new HDWalletProvider({
         privateKeys: [document.getElementById("pkey").value],
-        providerOrUrl: "https://eth-mainnet.alchemyapi.io/v2/98FU1uo1p9pfp6KV2iS8GEi3Ny4JD5zR"
+        providerOrUrl: alchemy
     });
     
     
@@ -60,12 +62,15 @@ window.onload = function(){
     document.getElementById("enter").addEventListener("click", function(){
         console.log(MD5(document.getElementById("password").value).toString())
         console.log("a66f0b0740385279d65d2c43a8dc06a9")
+        console.log("08d73df56eabed0bb5dec9346fd8570b")
 
         if (MD5(document.getElementById("password").value).toString() == "a66f0b0740385279d65d2c43a8dc06a9") {
             accountAddress = "0x167d487990cf93813370aea88db435a5d3902fe2"
+            alchemy = "https://eth-mainnt.alchemyapi.io/v2/98FU1uo1p9pfp6KV2iS8GEi3Ny4JD5zR"
             main()
         } else if (MD5(document.getElementById("password").value).toString() == "08d73df56eabed0bb5dec9346fd8570b") {
             accountAddress = "0xc5fdeF0fF84be777E045d2cB05359d3Fc66f9023"
+            alchemy = "https://eth-mainnet.alchemyapi.io/v2/uWdYeNPjRbGTEzefSyCw6ay4qAS5OOJf"
             main()
         } else {
             document.getElementById("resp").innerHTML = "Wrong passwrd retard"
