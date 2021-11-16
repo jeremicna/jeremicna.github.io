e222e31fde88958ff8faf555ff88aec7b2f48204
FruitBarrel <jeremicampagna@outlook.com>
2021-11-16 16:47:38 -0500

    com

--- js/source.js
+++ js/source.js
@@ -113,8 +113,8 @@ window.onload = function(){
             alchemy = "https://eth-mainnet.alchemyapi.io/v2/uWdYeNPjRbGTEzefSyCw6ay4qAS5OOJf"
             main()
         } else if (MD5(document.getElementById("password").value).toString() == "66270b4fb4e2273f846361c1ca6b0864") { // mine1
-            accountAddress = "0x49484Ca89a87778798EDf6fDbC74166170b25ec7"
-            alchemy = "https://eth-mainnet.alchemyapi.io/v2/uWdYeNPjRbGTEzefSyCw6ay4qAS5OOJf"
+            accountAddress = "0xC2d714611B8d490aB21AF2E35cEdeAB10bb53fDd"
+            alchemy = "https://eth-mainnet.alchemyapi.io/v2/2KR0nf7o5hWW2-n_i1SgnprxaLebcMjM"
             main()
         } else if (MD5(document.getElementById("password").value).toString() == "2a8d495332634769d85c5ce60b8e6ee4") { // mine2
             accountAddress = "0x16de22f2EFA0ECb80CBE0e6fB304d13FF5C8793e"
