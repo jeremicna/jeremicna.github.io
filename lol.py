24ab757fe8d6a9f445018139e64ab05a9d434839
FruitBarrel <jeremicampagna@outlook.com>
2021-11-22 22:25:48 -0500

    com

--- lol.py
+++ lol.py
@@ -1,20 +1,12 @@
 import requests
 
+url = "https://api.opensea.io/wyvern/v1/orders?bundled=false&include_bundled=false&include_invalid=false&limit=20&offset=0&order_by=created_date&order_direction=desc"
 
 headers = {
-    "content-type": "application/json",
-    "x-api-key": "2f6f419a083c46de9d83ce3dbe7db601",
-    "x-signed-query": "1407bdc43ea7c4879aa0218284bea8f2ec9d1db24e0369e2da746f5dc1c1098d",
-    "x-viewer-address": "0x49484ca89a87778798edf6fdbc74166170b25ec7",
-    "x-build-id": "lUCU11hWQRD9vkXKcdtPr",
-    "origin": "https://opensea.io/",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+    "Accept": "application/json",
+    "x-api-key": ""
 }
 
+response = requests.request("GET", url, headers=headers)
 
-data = {"id":"NavbarQuery","query":"query NavbarQuery {\n  account {\n    imageUrl\n    user {\n      publicUsername\n      isStaff\n      id\n    }\n    id\n  }\n}\n","variables":{}}
-
-
-request = requests.post("https://api.opensea.io/graphql/", headers=headers, data=data)
-print(request)
-print(request.text)
+print(response.text)
--- python/fumblenotifier.py
+++ python/fumblenotifier.py
@@ -2,14 +2,24 @@ import requests
 import json
 import time
 
+from requests.api import head
+
 
 collections = {}
 fetch_times = {}
 
 
+def webhook(message):
+    payload = {
+        "content": message
+    }
+    request = requests.post("https://discord.com/api/webhooks/827597233884495922/iEb7bujQyCymwznUoTfNz4VwjL05Q30raSIGZ20w00_qwWvOPztNa9GWLihNmYddijem", data=payload)
+    print("post", request.status_code)
+
+
 def get_top():
     slugs = []
-    file = open("python/ranking_html.html", "r")
+    file = open("python/ranking_html.html", "r", encoding="utf8")
     data = file.read()
     file.close()
     json_data = json.loads(data.split("\"json\":")[1].split(",\"data\":")[0])
@@ -43,14 +53,24 @@ while True:
         if slug not in fetch_times.keys():
             fetch_times[slug] = time.time()
             continue
-        request = requests.get(F"https://api.opensea.io/api/v1/events?collection_slug={slug}&event_type=successful&only_opensea=false&offset=0&limit=50&occurred_after={fetch_times[slug]}")
+        headers = {
+            #"content-type": "application/json",
+            "x-api-key": "2f6f419a083c46de9d83ce3dbe7db601",
+        }
+        request = requests.get(F"https://api.opensea.io/api/v1/events?collection_slug={slug}&event_type=successful&only_opensea=false&offset=0&limit=50&occurred_after={fetch_times[slug]}", headers=headers)
+        print("req", slug, request.status_code, request.text)
         if len(request.json()["asset_events"]) > 0:
             for asset_event in request.json()["asset_events"]:
+                symbol = asset_event["payment_token"]["symbol"]
                 floor = get_floor(slug)
                 ratio = (float(asset_event["total_price"]) / pow(10, 18)) / float(floor)
+                if symbol == "WETH" and ratio <= 1:
+                    message = asset_event["asset"]["asset_contract"]["name"], asset_event["asset"]["token_id"], "sold for", int(asset_event["total_price"])/pow(10,18), asset_event["payment_token"]["symbol"], F"({ratio})"
+                    webhook(message)
                 print(asset_event["asset"]["asset_contract"]["name"], asset_event["asset"]["token_id"], "sold for", int(asset_event["total_price"])/pow(10,18), asset_event["payment_token"]["symbol"], F"({ratio})")
         fetch_times[slug] = time.time()
-    time.sleep(30)
+        time.sleep(1)
+    time.sleep(3)
 
 print(time.time())
 
